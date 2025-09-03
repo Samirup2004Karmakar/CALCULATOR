5b851e23bb63a010ad3c53a78e732fac5a46f6f0
Samirup2004Karmakar <reek99karmakar@gmail.com>
2025-09-03 12:31:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let display = document.getElementById("display");
+
+function appendNumber(num) {
+  display.value += num;
+}
+
+function appendOperator(op) {
+  if (display.value === "") return;
+  let lastChar = display.value.slice(-1);
+  if ("+-*/".includes(lastChar)) {
+    display.value = display.value.slice(0, -1) + op;
+  } else {
+    display.value += op;
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
